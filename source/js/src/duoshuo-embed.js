680b1e2b802f8e23d4b7bbdce89ae544ebca8a98
karl zhou <zhoujiealex@gmail.com>
2016-12-19 22:10:14 +0800

    更新css

--- source/js/src/duoshuo-embed.js
+++ source/js/src/duoshuo-embed.js
@@ -19,11 +19,11 @@ function checkMobile() {
 //管理员判断开始
 function sskadmin(e) {
     var ssk = '';
-    if (e.user_id == 9365680) {
+    if (e.user_id == 6284895680469140000) {
         if (checkMobile()) {
-            ssk = '<span class="ua"><span class="sskadmin">博主</span></span><br><br>';
+            ssk = '<span class="ua"><span class="sskadmin">Root</span></span><br><br>';
         } else {
-            ssk = '<span class="ua"><span class="sskadmin">博主</span></span>';
+            ssk = '<span class="ua"><span class="sskadmin">Root</span></span>';
         }
     } else {
         if (checkMobile()) {
